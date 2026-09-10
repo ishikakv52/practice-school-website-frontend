523efcb691b840d6e9a7a5cb14675dbc2804e596
Ishika <ishikakv52@gmail.com>
2026-09-10 17:14:45 +0530

    Add SelfAttendance to Accountant Dashboard

--- src/app/accountant/dashboard/page.tsx
+++ src/app/accountant/dashboard/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import ProtectedDashboard from "@/components/ProtectedDashboard";
+import SelfAttendance from "@/components/SelfAttendance";
 import AccountantFeePanel from "@/components/AccountantFeePanel";
 
 export default function AccountantDashboardPage() {
   return (
     <ProtectedDashboard expectedRole="accountant" title="Accountant Dashboard">
+      <SelfAttendance />
       <AccountantFeePanel />
     </ProtectedDashboard>
   );
